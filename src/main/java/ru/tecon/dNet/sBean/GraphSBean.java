89dfc12ae9e61138eebbcb3a50de8fe870ceff7f
Maksim <shchelkonogov@mail.ru>
2019-10-14 13:58:51 +0300

    Почистил код

--- src/main/java/ru/tecon/dNet/sBean/GraphSBean.java
+++ src/main/java/ru/tecon/dNet/sBean/GraphSBean.java
@@ -14,8 +14,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.logging.Logger;
 
