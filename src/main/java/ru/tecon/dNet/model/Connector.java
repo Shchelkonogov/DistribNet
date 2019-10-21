9af180d43cf68abaf4553a40c4b52ff3fd856de5
Maksim <shchelkonogov@mail.ru>
2019-10-21 10:26:42 +0300

    Добавил темепературу в модель связей

--- src/main/java/ru/tecon/dNet/model/Connector.java
+++ src/main/java/ru/tecon/dNet/model/Connector.java
@@ -8,6 +8,7 @@ public class Connector implements Serializable {
 
     private String name;
     private String energy;
+    private ConnectorValue temperature;
     private ConnectorValue[] in = new ConnectorValue[3];
     private ConnectorValue[] out = new ConnectorValue[3];
     private ConnectorValue[] center = new ConnectorValue[3];
@@ -40,11 +41,20 @@ public class Connector implements Serializable {
         this.energy = energy;
     }
 
+    public ConnectorValue getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(ConnectorValue temperature) {
+        this.temperature = temperature;
+    }
+
     @Override
     public String toString() {
         return new StringJoiner(", ", Connector.class.getSimpleName() + "[", "]")
                 .add("name='" + name + "'")
                 .add("energy='" + energy + "'")
+                .add("temperature=" + temperature)
                 .add("in=" + Arrays.toString(in))
                 .add("out=" + Arrays.toString(out))
                 .add("center=" + Arrays.toString(center))
