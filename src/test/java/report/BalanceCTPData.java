42c0cf98b5eca85848010388431e19933b576a58
Maksim <shchelkonogov@mail.ru>
2020-09-29 09:12:49 +0300

    Revert: Merge remote-tracking branch 'origin/master'

--- src/test/java/report/BalanceCTPData.java
+++ src/test/java/report/BalanceCTPData.java
@@ -1,4 +1,4 @@
-package ru.tecon.dNet.report;
+package report;
 
 import ru.tecon.dNet.report.model.ConsumerModel;
 import ru.tecon.dNet.report.model.DataModel;
@@ -6,8 +6,9 @@ import ru.tecon.dNet.sBean.ReportBeanLocal;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
-public class Test implements ReportBeanLocal {
+public class BalanceCTPData implements ReportBeanLocal {
     
     public String getCTP(int object) {
         return "01-01-01";
@@ -29,6 +30,11 @@ public class Test implements ReportBeanLocal {
         return "Источник 1";
     }
 
+    @Override
+    public String getAddress(int objectID) {
+        return "ADDRESS";
+    }
+
     public List<ConsumerModel> getObjectNames(int object) {
         return Arrays.asList(new ConsumerModel("дом 1", 1), new ConsumerModel("дом 2", 2),
                 new ConsumerModel("дом 3", 3), new ConsumerModel("дом 4", 4));
@@ -36,18 +42,18 @@ public class Test implements ReportBeanLocal {
 
     @Override
     public List<DataModel> getInParameters(int object, String date) {
-        return Arrays.asList(new DataModel("Имя 1", "Значение 1", 1, 1), new DataModel("Имя 2", "Значение 2", 2, 2),
+        return Arrays.asList(new DataModel("Q", String.valueOf(ThreadLocalRandom.current().nextInt(-1000, 1000)), 1, 1), new DataModel("Имя 2", "Значение 2", 2, 2),
                 new DataModel("Имя 3", "Значение 3", 3, 3));
     }
 
     @Override
     public List<DataModel> getOutParameters(int object, String date) {
-        return Arrays.asList(new DataModel("Имя 1", "Значение 1", 1, 1), new DataModel("Имя 2", "Значение 2", 2, 2),
+        return Arrays.asList(new DataModel("Имя 1", "Значение 1", 1, 1), new DataModel("Q", String.valueOf(ThreadLocalRandom.current().nextInt(-1000, 1000)), 2, 2),
                 new DataModel("Имя 3", "Значение 3", 3, 3), new DataModel("Имя 4", "Значение 4", 4, 4));
     }
 
     @Override
-    public String getValue(int object, int id, int statId, String date) {
-        return String.valueOf(Math.random());
+    public String getValue(int parentID, int object, int id, int statId, String date) {
+        return String.valueOf(ThreadLocalRandom.current().nextInt(-1000, 1000));
     }
 }
